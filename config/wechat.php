94b0eafdc0af4ac5643f65ffce90b3c26e578ec6
周文刚 <php1108>
2017-10-19 17:24:41 +0800

    update

--- config/wechat.php
+++ config/wechat.php
@@ -16,9 +16,9 @@ return [
     /*
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'app_id'  => env('WECHAT_APPID', 'your-app-id'),         // AppID
-    'secret'  => env('WECHAT_SECRET', 'your-app-secret'),     // AppSecret
-    'token'   => env('WECHAT_TOKEN', 'your-token'),          // Token
+    'app_id'  => env('WECHAT_APPID', 'wx8c090e47413e84ec'),         // AppID
+    'secret'  => env('WECHAT_SECRET', 'wx8c090e47413e84ec'),     // AppSecret
+    'token'   => env('WECHAT_TOKEN', 'weixin'),          // Token
     'aes_key' => env('WECHAT_AES_KEY', ''),                    // EncodingAESKey
 
     /**
