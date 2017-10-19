d73fc7f204f10db78faebfc3b6c0fe7c0bd1c21c
周文刚 <php1108>
2017-10-19 15:04:03 +0800

    update

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -7,7 +7,19 @@ use EasyWeChat\Foundation\Application;
 class ApiController extends Controller
 {
     public function api(){
-        $app = new Application($this->options);
+        var $options = [
+            'debug'  => true,
+            'app_id' => 'wx8c090e47413e84ec',//'your-app-id',
+            'secret' => 'c144249dc9e67f9928bf921d1c69dfbf',//'you-secret',
+            'token'  => 'weixin',
+            // 'aes_key' => null, // 可选
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+            ],
+            //...
+        ];
+        $app = new Application($options);
         $response = $app->server->serve();
         // 将响应输出
         return $response;
