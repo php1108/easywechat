1e4032bb50696804856dd2e3475e6c89857e1937
周文刚 <php1108>
2017-10-19 15:11:46 +0800

    update

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -1,21 +1,24 @@
 <?php
 
 namespace App\Http\Controllers\API;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use EasyWeChat\Foundation\Application;
 
 class ApiController extends Controller
 {
-    public function api(){
-        var $options = [
-            'debug'  => true,
+    public function api()
+    {
+
+        $options = [
+            'debug' => true,
             'app_id' => 'wx8c090e47413e84ec',//'your-app-id',
             'secret' => 'c144249dc9e67f9928bf921d1c69dfbf',//'you-secret',
-            'token'  => 'weixin',
+            'token' => 'weixin',
             // 'aes_key' => null, // 可选
             'log' => [
                 'level' => 'debug',
-                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+                'file' => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
             ],
             //...
         ];
@@ -24,5 +27,6 @@ class ApiController extends Controller
         // 将响应输出
         return $response;
     }
+
 
 }
